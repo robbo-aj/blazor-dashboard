9c6ed8a70fddbee236e86e8c20b0591da7effd12
Marin Bratanov <m.bratanov@gmail.com>
2019-05-13 17:40:50 +0300

    move data service to the backend

---
+++ BlazorDashboard.Server/Controllers/IssuesController.cs
@@ -0,0 +1,21 @@
+﻿using BlazorDashboard.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorDashboard.Shared.Models;
+using BlazorDashboard.Server.Data;
+
+namespace BlazorDashboard.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class IssuesController : Controller
+    {
+        [HttpGet("[action]")]
+        public async Task<IEnumerable<Issue>> GetIssues(DateTime timeRange)
+        {
+            return await IssuesGenerator.GenerateIssues(timeRange);
+        }
+    }
+}
